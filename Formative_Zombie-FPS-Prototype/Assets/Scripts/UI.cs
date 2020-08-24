60ad1dbb4ce56132136b7f79f9bcae96f4d212e3
Kristian <hisfh67@gmail.com>
2020-08-24 04:37:18 +0200

    Cleaning up.

---
+++ Formative_Zombie-FPS-Prototype/Assets/Scripts/UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] private Text ammoLeftText;
+    [SerializeField] private Text bulletsInMagazineText;
+    
+    private Pistol pistolCs;
+
+    void Start()
+    {
+        pistolCs = new Pistol();
+    }
+
+    void Update()
+    {
+        ammoLeftText.text = $"Bullets left: {pistolCs.BulletsLeft}";
+        bulletsInMagazineText.text = $"Magazine: {pistolCs.LoadedBullets}";
+    }
+}
